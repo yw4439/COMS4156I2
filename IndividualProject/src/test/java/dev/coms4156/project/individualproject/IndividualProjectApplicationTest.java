e0b21df067a763f863c22ba6c028c10b0157c0d5
Yanming Wei <yw4439@columbia.edu>
2024-09-26 22:41:22 -0400

    slight change about code style

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/IndividualProjectApplicationTest.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/IndividualProjectApplicationTest.java
@@ -1,8 +1,8 @@
 package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
